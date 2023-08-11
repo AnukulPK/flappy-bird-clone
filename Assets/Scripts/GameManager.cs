9feb029d6bf105234fd41e9a0406d179e65afbba
Anukul <anukulpk2@gmail.com>
2023-08-11 22:21:12 +0530

    Implemented game over and menu implemented

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,19 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     private int score;
+    public Text scoreText;
+    public GameObject playButton;
+    public GameObject gameOver;
+    public PlayerController player;
+
+    private void Awake()
+    {
+        Application.targetFrameRate = 60;
+        Pause();
+    }
+
+    public void Play()
+    {
+        score = 0;
+        scoreText.text = score.ToString();
+
+        playButton.SetActive(false);
+        gameOver.SetActive(false);
+
+        Time.timeScale = 1f;
+        player.enabled = true;
+
+        PipesController[] pipes = FindObjectsOfType<PipesController>();
+
+        for(int i =0; i < pipes.Length; i++)
+        {
+            Destroy(pipes[i].gameObject);
+        }
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        player.enabled = false;
+    }
 
     public void GameOver()
     {
-        Debug.Log("Game Over");
+        gameOver.SetActive(true);
+        playButton.SetActive(true);
+
+        Pause();
     }
 
     public void IncreaseScore()
     {
         score++;
+        scoreText.text = score.ToString();
     }
 
 
